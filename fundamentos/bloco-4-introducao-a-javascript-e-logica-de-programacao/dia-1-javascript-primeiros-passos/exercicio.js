c3563b9715da4d2390af8bcea1ab11ae861ec81b
Jake Chien <jake_chien@outlook.com>
2022-03-07 19:31:57 -0300

    Adicionar operações matemáticos e retornar o maior valor entre dois numeros

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio.js
@@ -0,0 +1,25 @@
+const a = 28;
+const b = 52;
+
+/* Adição */
+let adicao = a + b;
+
+/* Subtração */
+let subtracao = a - b;
+
+/* Multiplicação */
+let multiplicacao = a * b;
+
+/* Divisão */
+let divisao = a / b;
+
+/* Módulo */
+let modulo = a % b;
+
+/* Retornando o maior valor entre dois numeros */
+if (a > b) {
+    console.log(a);
+}
+else if (a < b) {
+    console.log(b);
+}
