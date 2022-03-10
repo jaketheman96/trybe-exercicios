df4ff690680eb6749231d6d9e44cbdd520c10422
Jake Chien <jake_chien@outlook.com>
2022-03-10 20:46:39 -0300

    Exercicios 4.4 realizadas

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio.js
@@ -0,0 +1,97 @@
+/* 1 */
+console.log('Exercicio 1:');
+console.log(' ');
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+console.log('Bem-vinda, ' + info.personagem);
+
+/* 2 */
+console.log(' ');
+console.log('Exercicio 2: ');
+console.log(' ');
+
+info['recorrente'] = 'Sim';
+
+console.log(info)
+
+/* 3 */
+console.log(' ');
+console.log('Exercicio 3: ');
+console.log(' ');
+
+for (let index in info) {
+  console.log(index);
+}
+
+/* 4 */
+console.log(' ');
+console.log('Exercicio 4: ');
+console.log(' ');
+
+for (let index in info) {
+  console.log(info[index]);
+}
+
+/* 5 */
+console.log(' ');
+console.log('Exercicio 5: ');
+console.log(' ');
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim',
+};
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (let properties in info) {
+  if (
+    properties === 'recorrente' &&
+    info[properties] === 'Sim' &&
+    info2[properties] === 'Sim'
+  ) {
+    console.log('Ambos recorrentes');
+  } else {
+    console.log(info[properties] + ' e ' + info2[properties]);
+  }
+}
+
+/* 6 */
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0].titulo + '"');
+
+/* 7 */
+/* leitor.livrosFavoritos.push({
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+}) */
+
+/* 8 */
+if (leitor.livrosFavoritos.length === 1) {
+  console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livro favorito.');
+} else {
+  console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos.')
+}
