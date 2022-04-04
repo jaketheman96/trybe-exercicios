04ec9d4dbd5c1aaf12ae46e00f054bf07a40466a
Jake Chien <jake_chien@outlook.com>
2022-04-04 20:42:28 -0300

    Exercicio 1 parte 2 feito

---
+++ fundamentos/exercicios/exercicio-parte2/script.js
@@ -0,0 +1,54 @@
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      }
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+const customerInfo = (order) => {
+  const nameDelivery = order['name'];
+  const phone = order['phoneNumber'];
+  const street = order['address'].street;
+  const number = order['address'].number;
+  const apartment = order['address'].apartment;
+  console.log(`Olá Ana Silveira, entrega para: ${nameDelivery}, ${phone}, ${street}, ${number}, apartamento:${apartment}.`);
+
+}
+
+customerInfo(order);
+
+const orderModifier = (order) => {
+  // Adicione abaixo as informações necessárias.
+
+}
+
+orderModifier(order);
