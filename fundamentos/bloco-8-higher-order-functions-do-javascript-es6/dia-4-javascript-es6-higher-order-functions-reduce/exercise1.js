cc260925b86aa7a68551e0030b36ad25424389e3
Jake Chien <jake_chien@outlook.com>
2022-04-12 19:30:03 -0300

    Adiciona exercicio 2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
@@ -1,3 +1,4 @@
+/* 1 */
 const arrays = [
   ['1', '2', '3'],
   [true],
@@ -9,3 +10,76 @@ function flatten() {
 }
 // console.log(flatten());
 // function concat, para concatenar as infos em uma array.
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// Adicione o código do exercício aqui:
+
+/* 2 */
+function reduceNames() {
+  const reduceBooks = books.reduce((acc, book, index, array) => index === array.length - 1 ? `${acc} ${book.author.name}.` : `${acc} ${book.author.name},` , '');
+  return reduceBooks;
+}
+// console.log(reduceNames());
+// condicional ternary operator utilizado para adicionar a "." na última posição do array e "," nos restantes
